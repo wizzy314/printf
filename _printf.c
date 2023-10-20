d7f19ccefa050596db3cde9041324ba9c731e4fb
eaziwizzy <wisdomakatugba1@gmail.com>
2023-10-19 18:16:14 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,40 +1,60 @@
 #include "main.h"
 
 /**
-	*_printf - mimics printf.
-	*@format: identifier look for
-	*Return: integer.
-	*/
-
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ * @...: Variable number of arguments for format.
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
-	match m[] = {{"%c", print_char}, {"%s", print_str}, {"%%", print_pcent}};
+    va_list args;
+    int printed_chars = 0;
 
-	va_list args;
-	int i = 0;
-	int len = 0;
-	int j;
+    va_start(args, format);
 
-	va_start(args, format);
-	if (format == NULL || (format[0] ==  '%' && format[1] == '\0'))
-Here:
-	while (format[i] != '\0')
-	{
-		j = 2;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len = len + m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-		j++;
-		}
-		_putchar(format[i]);
-	i++;
-	len++;
-	}
-	va_end(args);
-	return (len);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+             write(1, format, 1);
+            printed_chars++;
+        }
+        else
+        {
+            format++;
+												if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                char *s = va_arg(args, char *);
+                if (s)
+                {
+                    write(1, s, strlen(s));
+                    printed_chars += strlen(s);
+                }
+                else
+                {
+                    write(1, "(null)", 6);
+                    printed_chars += 6;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                printed_chars++;
+            }
+	       }
+        format++;
+
+					}
+
+    va_end(args);
+
+    return printed_chars;
 }
+
